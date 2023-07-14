3aa03eda49703b83f9905f454a689f7c1dec233c
Xavier Arpa <arpaxavier@gmail.com>
2023-07-14 20:07:01 +0200

    -

---
+++ BeerJam2D/Assets/Code/PalleteConstants.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PalleteConstants 
+{
+    public static readonly Color c_verde_san_miguel = new Color(19,59,43);
+    public static readonly Color c_crema_san_miguel = new Color(232,225,213);
+    public static readonly Color c_tostado_san_miguel = new Color(173,124,89);
+    public static readonly Color c_amarillo_san_miguel = new Color(209,144,0);
+    public static readonly Color c_verde_claro_san_miguel = new Color(136,141,48);
+}
