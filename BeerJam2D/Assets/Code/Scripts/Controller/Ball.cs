2d57d6ea7adac37c9febeae40e6d84336b0a6498
Xavier Arpa <arpaxavier@gmail.com>
2023-07-15 18:02:28 +0200

    - fix ball bug

--- BeerJam2D/Assets/Code/Scripts/Controller/Ball.cs
+++ BeerJam2D/Assets/Code/Scripts/Controller/Ball.cs
@@ -21,9 +21,14 @@ public class Ball : MonoBehaviour
     public void LaunchBall()
     {
         // Lanzar la pelota en una dirección aleatoria
-        float randomDirectionX = Random.Range(-1f, 1f);
-        float randomDirectionY = Random.Range(-1f, 1f);
+        float randomDirectionX = MinMax(.5f, 1f);
+        float randomDirectionY = MinMax(.1f, 1f);
         Vector2 launchDirection = new Vector2(randomDirectionX, randomDirectionY);
         rb.AddForce(launchDirection * speed, ForceMode2D.Impulse);
+    }
+
+    private float MinMax(float min, float max) 
+    {
+        return (Random.Range(0, 2) == 0 ? 1 : -1) * Random.Range(min,max);
     }
 }
