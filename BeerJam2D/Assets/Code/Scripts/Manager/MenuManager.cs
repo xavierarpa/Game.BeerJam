c7a168d4304e283c6e11da35af2e17f8e5a43a3c
Xavier Arpa <arpaxavier@gmail.com>
2023-07-16 17:06:48 +0200

    -

--- BeerJam2D/Assets/Code/Scripts/Manager/MenuManager.cs
+++ BeerJam2D/Assets/Code/Scripts/Manager/MenuManager.cs
@@ -7,8 +7,14 @@ public class MenuManager : MonoBehaviour
     [Header("Settings")]
     [Space]
     public AudioClip clip_menu;
+    public Animator animator;
     private void Awake() 
     {
         AudioManager._.PlayMusic(clip_menu);
+        FadeManager._.target=0;
     }   
+    private void Start()
+    {
+        animator.SetTrigger("Display");
+    }
 }
