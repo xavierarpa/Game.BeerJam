e1ff54cd3e8ec2c10e74622483ddb4b007cc3e15
jasolflo <61027922+jasolflo@users.noreply.github.com>
2023-07-16 18:42:15 +0200

    Tutorial entero

--- BeerJam2D/Assets/Code/Scripts/Manager/TutorialManager.cs
+++ BeerJam2D/Assets/Code/Scripts/Manager/TutorialManager.cs
@@ -1,33 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
     public int _index;
     public Animator[] animators;
+    public Animator aux;
 
     private int index = 0;
     private bool canSlide = true;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) && canSlide)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0)) && canSlide)
         {
             Slide(1);
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow) && canSlide)
+        } /*else if (Input.GetKeyDown(KeyCode.LeftArrow) && canSlide)
         {
             Slide(-1);
-        }
+        }*/
     }
 
     public void Slide(int dir)
     {
-        Debug.Log("Slideando");
-        Debug.Log("El index actual es " + index);
-        if((index == 0 && dir == -1) || (index == 2 && dir == 1))
+
+
+        /*if((index == 0 && dir == -1) || (index == 2 && dir == 1))
         {
             //Nada
+        }*/
+        if (index == 2 && dir == 1)
+        {
+            SceneManager.LoadScene("Menu");
         }
         //dir == -1: slide fuera (der)
         else if(dir == 1)
@@ -48,7 +54,7 @@ public class TutorialManager : MonoBehaviour
     IEnumerator delay_to_slide()
     {
         canSlide = false;
-        yield return new WaitForSeconds(0.32f);
+        yield return new WaitForSeconds(0.4f);
         canSlide = true;
     }
 
