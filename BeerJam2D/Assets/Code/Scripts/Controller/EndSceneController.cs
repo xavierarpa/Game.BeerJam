0efa33a056504c1fb0b1fa190e437621d2940587
jasolflo <61027922+jasolflo@users.noreply.github.com>
2023-07-16 14:43:15 +0200

    Cambio de plato en animación final

--- BeerJam2D/Assets/Code/Scripts/Controller/EndSceneController.cs
+++ BeerJam2D/Assets/Code/Scripts/Controller/EndSceneController.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndSceneController : MonoBehaviour
 {
     public Animator animator;
     public GameObject text_pl1;
     public GameObject text_pl2;
+    public Image plate;
+    public Sprite[] plate_sprites;
+    
+    
     void Start()
     {
         FadeManager._.target = 0;
-        if(GameManager._.lastWinner == 1)
+        //if(GameManager._.lastWinner == 1)
+        if(true)
         {
             text_pl1.SetActive(true);
             text_pl2.SetActive(false);
@@ -21,5 +27,16 @@ public class EndSceneController : MonoBehaviour
             text_pl1.SetActive(false);
             animator.SetTrigger("Player2");
         }
+        SetPlateSprite();
     }
+
+    void SetPlateSprite()
+    {
+        int id = PlayerPrefs.GetInt("LastPlato", 0);
+        id++;
+        if(id >= plate_sprites.Length) id = 0;
+        PlayerPrefs.SetInt("LastPlato", id);
+        plate.sprite = plate_sprites[id];
+    }
+
 }
